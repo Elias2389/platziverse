a0a25623059fede8dda40c788ac93227109eb8a2
Andres <andres.rivas@mercadolibre.com>
2020-02-15 17:37:01 -0300

    Added inquirer and chalk

--- platziverse-db/setup.js
+++ platziverse-db/setup.js
@@ -1,7 +1,24 @@
 const debug = require('debug')('platziverse:db:setup')
 const db = require('./')
+const inquirer = require('inquirer');
+const chalk = require('chalk');
+
+const prompt = inquirer.createPromptModule();
 
 async function setup () {
+  const answer = await prompt([
+    {
+      type: 'confirm',
+      name: 'setup',
+      message: 'This will destroy your database, are you sure?'
+    }
+  ]);
+
+  if (!answer.setup) {
+    return console.log('Nothing happened!');
+  }
+
+
   const config = {
     database: process.env.DB_NAME || 'platziverse',
     username: process.env.DB_USER || 'platzi',
@@ -19,7 +36,7 @@ async function setup () {
 }
 
 function handleFatalError (err) {
-  console.error(err.message)
+  console.error(`${chalk.red('[Fatal error ]')} ${err.message}`)
   console.error(err.stack)
   process.exit(1)
 }
